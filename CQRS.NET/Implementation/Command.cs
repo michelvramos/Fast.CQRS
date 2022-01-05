f7f16d2d482b194b75454aed33784b7f431fb3c8
michelvramos <michelramos@hotmail.com.br>
2022-01-05 10:42:20 -0300

    added .net framework version

---
+++ CQRS.NET/Implementation/Command.cs
@@ -0,0 +1,15 @@
+﻿using CQRS.Interfaces;
+
+namespace CQRS.Implementation
+{
+    /// <summary>
+    /// Abstract Command class. All commands must derive from this class.
+    /// </summary>
+    public abstract class Command : Notifiable, ICommand
+    {
+        /// <summary>
+        /// Performs validation on input paremeters.
+        /// </summary>
+        public abstract void Validate();
+    }
+}
---
+++ CQRS.NET/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Fast CQRS .NET")]
+[assembly: AssemblyDescription("Easy to use implementation of Command Query Responsibility Segregation, aka CQRS.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Michel Vaz Ramos")]
+[assembly: AssemblyProduct("Fast CQRS .NET")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("93e82695-67b2-49ac-b52d-1ece63bcfb10")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.3.2")]
+[assembly: AssemblyFileVersion("1.0.3.2")]
